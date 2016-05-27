14b5565e76ae4d1c25e0c25091e93ad264f83f11
Alan <chin.alan.q@gmail.com>
2016-05-27 03:29:19 -0500

    git gud; added robot.py; astar is perfect :^)

--- scripts/robot.py
+++ scripts/robot.py
@@ -1,3 +1,30 @@
 #!/usr/bin/env python
 
 #robot.py implementation goes here
+from astar import AStar
+import rospy
+
+from std_msgs.msg import *
+
+class Robot():
+    def __init__(self):
+        rospy.init_node('Robot', anonymous=True)
+
+        self.sim_pub = rospy.Publisher(
+            "map_node/sim_complete",
+            Bool,
+            queue_size = 15
+        )
+
+        rospy.sleep(1)
+
+        AStar()
+        # MDP goes here
+
+        rospy.sleep(1)
+        self.sim_pub.publish(True)
+        rospy.sleep(1)
+        rospy.signal_shutdown("Done.")
+
+if __name__ == '__main__':
+    r = Robot()

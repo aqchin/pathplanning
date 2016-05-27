5e4db9b8acc2dab7bf6e7a62b2963753fabc157a
Dhruv Kaushal <dhruvrrp@gmail.com>
2016-05-27 18:08:58 -0500

    MDP initial commit

--- scripts/mdp.py
+++ scripts/mdp.py
@@ -1,1 +1,78 @@
-# mdp implementation needs to go herefrom read_config import read_configimport rospyfrom cse_190_assi_3.msg import PolicyListclass MDP():    EMPTY = 0    START = 1    WALL = 2    PIT = 3    GOAL = 4    def __init__(self):        self.config = read_config()        self.pl_pub = rospy.Publisher(            "results/policy_list",            PolicyList,            queue_size = 10        }        # get configs        map_size = self.config["map_size"]        start = self.config["start"]        goal = self.config["goal"]        walls = self.config["walls"]        pits = self.config["pits"]        # set up maps        # perform mdpif __name__ = '__main__':    MDP()
+# mdp implementation needs to go here
+def mdp ():
+    
+    config = read_config()
+    move_list = self.config["move_list"]
+    map_size = self.config["map_size"]
+    start =
+    goal =
+    walls =
+    pits = 
+    end_states = pits
+    end_states.append(goal)
+    current_pos = start
+    cur_val = defaultdict(lambda: 0)
+    cur_val[goal] = reward_for_reaching_goal
+    for i in pits:
+        cur_val[i] = reward_for_falling_in_pit
+    tmp_val = defaultdict(lambda: 0)
+    for i in range(max_iterations):
+        
+
+        for r in range(map_size[0]):
+            for c in range(map_size[1]):
+                hululu(r,c):   
+         
+        for i in cur_val.keys():
+            cur_val(i) = tmp_val(i)
+def hululu(r, c):
+    cur_pos = [r,c]
+    cur_best = 0
+    for i in move_list:
+        move_sum = 0
+        for pr in move_list: 
+
+            tmp_pos = cur_pos + pr
+            pr_s = set(pr)
+            prob = 0.0
+            if eql(pr, i):
+                prob = prob_move_forward
+            elif eql(([-x for x in i]), pr):
+                prob = prob_move_backward
+            else:
+                if i[1] == 0:
+                    tmp = [i[1], i[0]]
+                    if eql(tmp, pr):
+                        prob = prob_move_left
+                    else:
+                        prob = prob_move_right
+                else:
+                    tmp = [-i[1]. i[0]]
+                    if eql(tmp, pr):
+                        prob = prob_move_left
+                    else:
+                        prob = prob_move_right
+            if tmp_pos[0] < 0 or tmp_pos[1] < 0 or tmp_pos in walls:
+                new_val = prob*(reward_for_hitting_wall + discount * cur_val[cr_pos]
+            elif tmp_pos[0] > map_size[0] or tmp_pos[1] > map_size[1]:
+                new_val = prob*(reward_for_hitting_wall + discount * cur_val[cr_pos]
+            elif tmp_pos in pits:
+                new_val = prob * (reward_for_falling_in_pit + reward_for_each_step + discount * cur_val[tmp_pos])
+            elif eql(tmp_pos, goal):
+                new_val = prob * (reward_for_falling_in_pit + reward_for_each_step + discount * cur_val[tmp_pos])
+            else:
+                new_val = prob * (reward_for_each_step + discount * cur_val[tmp_pos])
+            move_sum += new_val
+        
+        if move_sum > cur_best:
+            cur_best = move_sum
+    temp_val(cur_pos) = cur_best
+
+            
+                                    
+
+def eql(a, b):
+    if sum(i != j for i, j in zip(a, b)) > 0:
+        return False
+    else:
+        return True
